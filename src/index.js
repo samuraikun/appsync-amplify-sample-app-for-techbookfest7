ee5f6060baadeb80831a5dc201ffe6a63dc2680f
samuraikun <tazitaziawawa@gmail.com>
2019-09-17 17:08:24 +0900

    connect react to amplify

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
 import 'element-theme-default';
+
+Amplify.configure(aws_exports);
+console.log(Amplify);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
