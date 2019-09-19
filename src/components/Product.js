c44141be042b6464458978c0ea4341ca02e55e59
samuraikun <tazitaziawawa@gmail.com>
2019-09-20 03:49:42 +0900

    作成したProductモデルを表示する

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,178 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { API, graphqlOperation } from 'aws-amplify';
+import { S3Image } from 'aws-amplify-react';
+import {
+  Notification,
+  Popover,
+  Button,
+  Dialog,
+  Card,
+  Form,
+  Input
+} from 'element-react';
+import { updateProduct, deleteProduct } from '../graphql/mutations';
 
-function Product() {
+function Product({ product }) {
+  const [updateProductDialog, setUpdateProductDialog] = useState(false);
+  const [deleteProductDialog, setDeleteProductDialog] = useState(false);
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState(null);
+  const [shipped, setShipped] = useState(false);
+
+  const handleEditProduct = () => {
+    setUpdateProductDialog(true);
+    setDescription(product.description);
+    setShipped(product.shipped);
+    setPrice(product.price);
+  }
+
+  const handleUpdateProduct = async productId => {
+    try {
+      setUpdateProductDialog(false);
+      const input = {
+        id: productId,
+        description,
+        shipped,
+        price
+      }
+
+      const result = await API.graphql(graphqlOperation(updateProduct, { input }));
+      console.log('Update product', result)
+      
+      Notification({
+        title: 'success',
+        message: 'Product successfully updated!',
+        type: 'success',
+        duration: 2000
+      });
+    } catch(err) {
+      console.error(`Failed to updated product with id: ${productId}`, err);
+    }
+  }
+
+  const handleDeleteProduct = async productId => {
+    try {
+      setDeleteProductDialog(false);
+      const input = { id: productId };
+      await API.graphql(graphqlOperation(deleteProduct, { input }));
+
+      Notification({
+        title: 'success',
+        message: 'Product successfully deleted!',
+        type: 'success',
+        duration: 2000
+      });
+    } catch(err) {
+      console.error(`Failed to deleted product with id: ${productId}`, err);
+    }
+  }
+
   return (
-    <div>Product</div>
-  )
+    <div className='card-container'>
+      <Card bodyStyle={{ padding: 0, minWidth: '200px' }}>
+        <S3Image
+          imgKey={product.file.key}
+          theme={{
+            photoImg: { maxWidth: '100%', maxHeight: '100%' }
+          }}
+        />
+        <div className='card-body'>
+          <h3 className='m-0'>{product.description}</h3>
+          <div className='text-right'>
+            <span className='mx-1'>
+              ¥{product.price}
+            </span>
+          </div>
+        </div>
+      </Card>
+      {/* Update / Delete Product Buttons */}
+      <div className='text-center'>
+        <>
+          <Button
+            type='warning'
+            icon='edit'
+            className='m-1'
+            onClick={() => handleEditProduct()}
+          />
+          <Popover
+            placement='top'
+            width='160'
+            trigger='click'
+            visible={deleteProductDialog}
+            content={
+              <>
+                <p>Do you want to delete this?</p>
+                <div className='text-right'>
+                  <Button
+                    size='mini'
+                    type='text'
+                    className='m-1'
+                    onClick={() => setDeleteProductDialog(false)}
+                  >
+                    キャンセル
+                  </Button>
+                  <Button
+                    type='primary'
+                    size='mini'
+                    className='m-1'
+                    onClick={() => handleDeleteProduct(product.id)}
+                  >
+                    削除
+                  </Button>
+                </div>
+              </>
+            }
+          >
+            <Button
+              type='danger'
+              icon='delete'
+              onClick={() => setDeleteProductDialog(true)}
+            />
+          </Popover>
+        </>
+      </div>
+
+      {/* Update Product Dialog */}
+      <Dialog
+        title='Update Product'
+        size='large'
+        customClass='dialog'
+        visible={updateProductDialog}
+        onCancel={() => setUpdateProductDialog(false)}
+      >
+        <Dialog.Body>
+          <Form labelPosition='top'>
+            <Form.Item label='Update Description'>
+              <Input
+                type='text'
+                icon='information'
+                placeholder='Description'
+                trim={true}
+                value={description}
+                onChange={description => setDescription(description)}
+              />
+            </Form.Item>
+            <Form.Item label='Update Price'>
+              <Input
+                type='number'
+                icon='plus'
+                placeholder='Price ($USD)'
+                value={price}
+                onChange={price => setPrice(price)}
+              />
+            </Form.Item>
+          </Form>
+        </Dialog.Body>
+        <Dialog.Footer>
+          <Button
+            onClick={() => handleUpdateProduct(product.id)}
+          >
+            Update
+          </Button>
+        </Dialog.Footer>
+      </Dialog>
+    </div>
+  );
 }
 
 export default Product;
